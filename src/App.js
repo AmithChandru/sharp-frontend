fa9f0d63041994f727cdf6ebbafb1705197eb07a
AmithChandru <amithc279@gmail.com>
2023-02-16 17:45:59 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import './App.css';
 import ExpenseItem from './Components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: 'Bangalore'
+    },
+    { 
+      id: 'e2',
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12),
+      location: 'Mumbai'
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: 'Delhi'
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: 'Chennai'
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem expenseDate={item.date} expenseTitle={item.title} expenseLocation={item.location} expenseAmount={item.amount} />
+        )
+      })}
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 2, 16);
+function ExpenseItem(props) {
+  /* const expenseDate = new Date(2023, 2, 16);
   const expenseTitle = 'Food';
   const expenseAmount = 200;
-  const expenseLocation = 'Bangalore';
+  const expenseLocation = 'Bangalore'; */
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <h2>{expenseLocation}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.expenseTitle}</h2>
+        <h2>{props.expenseLocation}</h2>
+        <div className='expense-item__price'>{props.expenseAmount}</div>
       </div>
     </div>
   )
