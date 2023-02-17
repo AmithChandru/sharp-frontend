21814c6f137e14e8ff2b978c72ca4ceed24cb9d2
AmithChandru <amithc279@gmail.com>
2023-02-17 16:33:27 +0530

    event listeners

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -4,10 +4,16 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+  const onButtonClick = () => {
+    let root = document.getElementById('item');
+    root.remove();
+  }
+
   return (
-    <div className="expense-item">
+    <div className="expense-item" id='item'>
       <ExpenseDate expenseDate={props.expenseDate} />
       <ExpenseDetails expenseTitle={props.expenseTitle} expenseAmount={props.expenseAmount} expenseLocation={props.expenseLocation} />
+      <button id='button' onClick={onButtonClick}>Delete Element</button>
     </div>
   )
 }
