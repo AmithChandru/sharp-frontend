46febac22a36aebcca76bed2df412ea113392f40
AmithChandru <amithc279@gmail.com>
2023-02-18 09:53:41 +0530

    keys

--- src/App.js
+++ src/App.js
@@ -1,86 +1,52 @@
-import { useState } from 'react';
-import './App.css';
-import ExpenseForm from './Components/ExpenseForm';
-import ExpenseItem from './Components/ExpenseItem';
+import React, { useState } from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-  const [expenses, setExpenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'Bangalore'
-    },
-    { 
-      id: 'e2',
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12),
-      location: 'Mumbai'
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: 'Delhi'
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: 'Chennai'
-    },
-  ]);
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  /* var expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'Bangalore'
-    },
-    { 
-      id: 'e2',
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12),
-      location: 'Mumbai'
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: 'Delhi'
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: 'Chennai'
-    },
-  ]; */
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addItem = (item) => {
-    setExpenses([...expenses, item]);
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
-    <div className="App">
-      <ExpenseForm addItem={addItem} />
-      {expenses.map((item) => {
-        return (
-          <ExpenseItem expenseDate={item.date} expenseTitle={item.title} expenseLocation={item.location} expenseAmount={item.amount} />
-        )
-      })}
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-  const month = props.expenseDate.toLocaleString('en-US', { month: 'long' });
-  const day = props.expenseDate.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.expenseDate.getFullYear();
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
 
   return (
     <div className='expense-date'>
@@ -11,7 +13,7 @@ const ExpenseDate = (props) => {
       <div className='expense-date__year'>{year}</div>
       <div className='expense-date__day'>{day}</div>
     </div>
-  )
-}
+  );
+};
 
 export default ExpenseDate;
